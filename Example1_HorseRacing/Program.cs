eaaf47aa9bbfd57222f240aff497d1cb19177d4e
kjhun95 <2c_2c@naver.com>
2022-12-21 21:52:45 +0900

    asdf

--- Example1_HorseRacing/Program.cs
+++ Example1_HorseRacing/Program.cs
@@ -36,21 +36,64 @@ namespace Example1_HorseRacing
 
         static void Main(string[] args)
         {
-            bool gameFinished = flase;
-
+            bool gameFinished = false;
+            Horse[] horses = new Horse[5];
+            Horse[] horsesFinished = new Horse[5];
+            int grade = 0;
+            int sec = 0;
             // 말 다섯마리 만들고
             // 이름 다 설정 해줌 (이름 패턴은 경주마1, 경주마2, 경주마3 ... )
+            for (int i = 0 < horses.Length; i++)
+            {
+                horses[i] = new Horse();
+                horses[i].Name = $"경주마{i + 1}";
+
+            }
+                Console.WriteLine($"==============={sec} 초 경과======================")
 
             while (gameFinished == false)
             {
+
                 // 각 말은 초당 10 ~ 20 (정수형) 범위의 거리를 랜덤하게 전진
                 // 각각의 말은 거리 200에 도달하면 도착해서 더이상 전진하지 않고
                 // 매초 각 말들이 아직 달리고 있다면 달린 거리를, 도착했다면 도착 상태를 콘솔창에 출력한다.
                 // 모든 말이 도착했으면 게임 끝냄
+                for (int i = 0; i < horses.Length; i++)
+                {
+                    // 경주가 끝난 말
+                    if (horses[i].IsFinished)
+                    {
+                        Console.WriteLine($"{horses[i].Name} 은 도착했다.");
+                    }
+                    //경주가 아직 끝나지 않은말은 달리게한다.
+                    else
+                    {
+                        random = new Random();
+                        horses[i].Run(random.Next(minSpeed, maxSpeed + 1));
+
+                        // 전진했는데 도착했으면 끝났다고 해줌
+                        if (horses[i].TotalDistance >= goalPos)
+                        {
+                            horses[i].IsFinished = true;
+                            horsesFinished[grade++] = horses[i];
+                    }
+                }
+                    // 모든말이 도착했는지 체크
+                    if (grade >= horses.Length)
+                        break;
+
+                    Thread.Sleep(1000);
+                    sec++;
                 random = new Random();
                 int tmpspeed = random.Next(minSpeed, maxSpeed + 1);
                 Thread.Sleep(1000);
             }
+
+            Console.WriteLine("===================경주 끝======================")
         }   // 각 말들을 등수 순서대로 이름을 출력해줌
+            for (int i = 0; i < horsesFinished.Length; i++)
+            {
+                Console.WriteLine($"{i + 1} 등 : {horsesFinished[i].Name}");
+            }
     }
 }
---
+++ Structure/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Reflection.Emit;
+
+// 구조체 사용자 정의 자료형
+// 멤버들을 캡슐화하는 자료형
+// 멤버 변수, 프로퍼티, 함수 ..
+//
+// 선언 형태
+// struct 이름
+// {
+//    멤버들 선언/정의
+// }
+
+namespace Structure
+{
+    public struct Stats
+    {
+        public int STR;
+        public int DEX;
+        public int INT;
+        public int LUK;
+
+        public int GetCombatPwer()
+        {
+            return STR
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 클래스는 참조타입
+            Player player = new Player();
+            player.STR = 10;
+            player.DEX = 10;
+            player.INT = 10;
+            player.LUK = 10;
+
+
+            // 그러면은 언제 클래스를 쓰고 언제 구조체를 쓰냐?
+            // 1. 멤버들의 크기 총합이 16 byte 이상일 때는 일반적으로 클래스를 씀
+            //
+            // 참조타입으로 값을 읽고 쓰는거 보다 값타입을 읽고 쓰는게 빠르다.
+            // 하지만 16 byte를 초과할때는 레지스터가 두번 이상 값을 읽어야 하기 때문에
+            // 값타입이어도 참조타입보다 느려진다.
+            //
+            // 2. 값을 쓰고 읽는것이 빈번하게 일어날 경우 (ex. 함수의 인자로 넘겨주는 횟수가 잦을 경우)
+            // 구조체를 사용하는것이 효율적이다.
+
+
+            // 구조체는 값타입
+            Player_s player_s = new Player_s();
+            enemy.Stats.STR = 10;
+            enemy.Stats.DEX = 10;
+            enemy.Stats.INT = 10;
+            enemy.Stats.LUK = 10;
+            Console.WriteLine($"적의 전투력이 {enemy.Stats.GetCombatPower() 입니다 !!");
+
+            Player_s player s = new Player_s();
+            Player_
+        }
+    }
+
+    public class Player
+    {
+        public int STR;
+        public int DEX;
+        public int INT;
+        public int LUK;
+    }
+
+    public struct 
+
+    public class Enemy
+    {
+        public Stats Stats;
+    }
+}
